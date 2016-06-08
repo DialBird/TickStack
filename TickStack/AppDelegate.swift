91b37d55245b7572d4b5fb02a048c7fdf12a1ba7
DialBird <rememberthatk@icloud.com>
2016-06-08 21:57:07 +0900

    appDelegateないの余分な部分を削除 解説を追加

--- TickStack/AppDelegate.swift
+++ TickStack/AppDelegate.swift
@@ -12,6 +12,8 @@ import RealmSwift
 //サイズを格納
 let screenSize: CGSize = UIScreen.mainScreen().nativeBounds.size
 let screenStr: String = "width: \(screenSize.width) height: \(screenSize.height)"
+
+//使っているデバイスが4sならばtrue
 var thisIs4s: Bool = false
 
 
@@ -74,45 +76,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         TaskDataSourceManager.sharedInstance.checkStoredDataExists()
         
         
-        if let notification = launchOptions?[UIApplicationLaunchOptionsLocalNotificationKey] as? UILocalNotification,let userInfo = notification.userInfo{
-            application.applicationIconBadgeNumber = 0
-            application.cancelLocalNotification(notification)
-        }
-        //復帰に関係なくバッジが0じゃなければ0にする
-        if application.applicationIconBadgeNumber != 0{
-            application.applicationIconBadgeNumber = 0
-        }
-        
-        
-        //デモ用------------------------------------------------------
-        //        try! realm.write({
-        //            let task1 = TaskCellData()
-        //            task1.taskName = "英語"
-        //            task1.taskGoalMinute = 30
-        //            task1.todaySecondStock = 11
-        //
-        //            let task2 = TaskCellData()
-        //            task2.taskName = "読書"
-        //            task2.taskGoalMinute = 10
-        //            task2.todaySecondStock = 5
-        //
-        //            let task3 = TaskCellData()
-        //            task3.taskName = "散歩"
-        //            task3.taskGoalMinute = 10
-        //            task3.todaySecondStock = 600
-        //            taskCellDataList.list.append(task1)
-        //            taskCellDataList.list.append(task2)
-        //            taskCellDataList.list.append(task3)
-        //
-        //            let demo = TaskDataSource()
-        //            demo.firstDay = NSDate()
-        //            demo.taskName = "散歩"
-        //            demo.numOfPassedDate = 110
-        //            demo.numOfSuccess = 93
-        //            demo.stockedTime = 120324
-        //            taskDataSourceList.list.append(demo)
-        //        })
-        
         //スリープモードに入らないようにする
         UIApplication.sharedApplication().idleTimerDisabled = true
         
@@ -120,55 +83,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
         NSNotificationCenter.defaultCenter().postNotificationName("applicationWillResignActive", object: nil)
     }
     
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        application.cancelAllLocalNotifications()
-        let notification = UILocalNotification()
-        notification.alertAction = "アプリを開く"
-        notification.alertBody = "やあけいすけ"
-        notification.fireDate = NSDate(timeIntervalSinceNow: 3)
-        notification.soundName = UILocalNotificationDefaultSoundName
-        notification.applicationIconBadgeNumber = 999999999
-        notification.userInfo = ["notifyID": "keisuke"]
-//        application.scheduleLocalNotification(notification)
     }
     
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-        if application.applicationIconBadgeNumber != 0{
-            application.applicationIconBadgeNumber = 0
-            print("application\(application.applicationIconBadgeNumber)")
-        }
     }
     
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         NSNotificationCenter.defaultCenter().postNotificationName("applicationDidBecomeActive", object: nil)
     }
     
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
     func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
-        if application.applicationState != .Active{
-            //バッジを０にする
-            application.applicationIconBadgeNumber = 0
-            //通知領域から削除する
-            application.cancelLocalNotification(notification)
-        }else{
-             //active時に通知が来たときはそのままバッジを0に戻す
-            if application.applicationIconBadgeNumber != 0{
-                application.applicationIconBadgeNumber = 0
-                application.cancelLocalNotification(notification)
-            }
-        }
     }
 }
 
